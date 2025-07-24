45922822623d9e2a6913606bbbdd8aac672ac046
Stephen Giang <StephenG753@Gmail.com>
2025-07-23 20:48:31 -0700

    testing to see if works

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Image style={styles.tinyLogo} source={require('./assets/icon.png')} />
+			<Text>Product</Text>
+			<StatusBar style="dark" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fffs',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	tinyLogo: {
+		width: 150,
+		height: 150,
+		marginBottom: 50,
+	},
 });
